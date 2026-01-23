96188b01362a5d845e6a43c12870c87412995791
ErickDKnight <erickmontesdk@gmail.com>
2026-01-23 11:49:53 -0800

    feat: implemented roles for users and allowed django admin form

---
+++ users/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('full_name', 'username', 'email', 'role')
+    list_filter = ('role',)
+    search_fields = ('username', 'email')
+    ordering = ('username',)
+
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('role',)}),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('role', 'email', 'first_name', 'last_name'),
+        }),
+    )
+
+    list_display = ('full_name', 'username', 'email', 'role', 'is_active', 'is_staff')
+admin.site.register(User, CustomUserAdmin)
---
+++ users/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class User(AbstractUser):
+    ADMIN = 'ADMIN'
+    OPERATOR = 'OPERATOR'
+    DELIVERY = 'DELIVERY'
+    
+    ROLE_CHOICES = (
+        (ADMIN, 'Admin'),
+        (OPERATOR, 'Operator'),
+        (DELIVERY, 'Delivery'),
+    )
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default=DELIVERY)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - ({self.role})"
+
+    @property
+    def full_name(self):
+        return f"{self.last_name}, {self.first_name}"
